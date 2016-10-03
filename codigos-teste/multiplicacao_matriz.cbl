adb385185980f13cd33df29f0d148ef7153368d6
alisonorita <keijo.alison@hotmail.com>
2016-10-03 17:18:58 -0300

    Adicionado Multiplicação de Matriz
    Adicionado Multiplicação de Matriz:
    
    - Aceita Matriz com linhas e colunas de 0 a 9
    - Aceita apenas valores de 0 a 9 nos elementos

---
+++ codigos-teste/multiplicacao_matriz.cbl
@@ -0,0 +1,89 @@
+      ******************************************************************
+      * Author: GRUPO-COBOL
+      * Date: 03/10/2016
+      * Purpose: MULTIPLICAR MATRIZ
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MULTIPMATRIZ.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       01 MAL PIC 9(1).
+       01 MAC PIC 9(1).
+       01 MBL PIC 9(1).
+       01 MBC PIC 9(1).
+       01 ARRAY.
+           02 X OCCURS 10 TIMES.
+               03 Y OCCURS 10 TIMES.
+                   04 A PIC 9(3).
+                   04 B PIC 9(3).
+                   04 R PIC 9(3).
+       01 L_A PIC 9(1).
+       01 C_A PIC 9(1).
+       01 L_B PIC 9(1).
+       01 C_B PIC 9(1).
+       01 VALOR PIC 9(3).
+       01 ELEMENTO PIC 9(3).
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY "Informe a quantidade de linhas da matriz A: ".
+           ACCEPT MAL.
+           DISPLAY "Informe a quantidade de colunas da matriz A: ".
+           ACCEPT MAC.
+           DISPLAY "Informe a quantidade de linhas da matriz B: ".
+           ACCEPT MBL.
+           DISPLAY "Informe a quantidade de colunas da matriz B: ".
+           ACCEPT MBC.
+
+           DISPLAY " ".
+           DISPLAY " ".
+
+           IF MAC NOT EQUAL MBL THEN
+               DISPLAY "A matriz resultante nao existe!"
+               DISPLAY ".. Definicao: so existe matriz produto A * B .."
+               DISPLAY ".. se o numero de colunas da matriz A .."
+               DISPLAY ".. for igual ao numero de linhas da matriz B .."
+               STOP RUN
+           END-IF.
+
+           DISPLAY "Informe os elementos da matriz A: ".
+           PERFORM READ-A VARYING L_A FROM 1 BY 1 UNTIL L_A > MAL
+               AFTER C_A FROM 1 BY 1 UNTIL C_A > MAC.
+
+           DISPLAY "Informe os elementos da matriz B: ".
+           PERFORM READ-B VARYING L_B FROM 1 BY 1 UNTIL L_B > MBL
+               AFTER C_B FROM 1 BY 1 UNTIL C_B > MBC.
+
+           DISPLAY " ".
+           DISPLAY " ".
+
+           DISPLAY "A matriz resultante R: ".
+           PERFORM CALC VARYING L_A FROM 1 BY 1 UNTIL L_A > MAL
+               AFTER C_B FROM 1 BY 1 UNTIL C_B > MBC.
+
+           PERFORM DSP-E VARYING L_A FROM 1 BY 1 UNTIL L_A > MAL
+               AFTER C_B FROM 1 BY 1 UNTIL C_B > MBC.
+
+           STOP RUN.
+
+       READ-A.
+           DISPLAY "("L_A","C_A") : ", ACCEPT A(L_A, C_A).
+
+       READ-B.
+           DISPLAY "("L_B","C_B") : ", ACCEPT B(L_B, C_B).
+
+       CALC.
+           MOVE 0 TO ELEMENTO.
+           PERFORM CALC-E VARYING C_A FROM 1 BY 1 UNTIL C_A > MAC
+               AFTER L_B FROM 1 BY 1 UNTIL L_B > MBL.
+           MOVE ELEMENTO TO R(L_A,C_B).
+
+       CALC-E.
+           COMPUTE VALOR = A(L_A,C_A) * B(L_B,C_B).
+           ADD VALOR TO ELEMENTO.
+
+       DSP-E.
+           DISPLAY "("L_A","C_B") = "R(L_A,C_B).
+
+       END PROGRAM MULTIPMATRIZ.
